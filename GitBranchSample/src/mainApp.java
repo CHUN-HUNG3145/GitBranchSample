865595cfe645c70c0172ac409d616fed347eb7d3
au0692045 <au0692045@gmail.com>
2021-06-08 16:09:13 +0800

    Add Math Branch

--- GitBranchSample/src/mainApp.java
+++ GitBranchSample/src/mainApp.java
@@ -4,6 +4,15 @@ public class mainApp {
 	public static void main(String[] args) {
 		//這是第一個版本
 		System.out.println("1st version");
+		Math cMath = new Math();
+		System.out.println();
+		System.out.println(cMath.add(10, 20));
 	}
 
+}
+
+class Math{
+	int add(int x, int y) {
+		return x+y;
+	}
 }
